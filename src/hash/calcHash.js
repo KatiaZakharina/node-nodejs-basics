39068db1ab92388e2fedf044aabe64665f290900
Katia <katia.zakharina@gmail.com>
2022-06-23 21:08:06 +0300

    feat: add hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,26 @@
+import crypto from 'crypto';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs/promises';
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  const destination = path.join(
+    __dirname,
+    'files',
+    'fileToCalculateHashFor.txt'
+  );
+
+  let file = null;
+  try {
+    file = await fs.readFile(destination);
+  } catch (error) {
+    throw error;
+  }
+
+  const hash = crypto.createHash('sha256').update(file).digest('hex');
+  console.log(hash);
 };
+
+calculateHash();
