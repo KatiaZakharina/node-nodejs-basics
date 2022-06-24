d9734561f7bd1c66ba4e735235a757751c584d4e
Katia <katia.zakharina@gmail.com>
2022-06-25 00:23:40 +0300

    feat: add cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,26 @@
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const destination = join(__dirname, 'files', 'script.js');
+
+  const childProcess = spawn('node', [destination, ...args]);
+
+  childProcess.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
+
+  process.stdin.on('data', (data) => {
+    childProcess.stdin.write(data.toString().trim());
+  });
+
+  childProcess.on('close', (data) => {
+    process.exit(data);
+  });
 };
+
+// node ./src/cp/cp.js arg1 arg2 arg3 arg4
+const argsArray = process.argv.slice(2);
+spawnChildProcess(argsArray);
